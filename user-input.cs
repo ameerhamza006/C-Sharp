de13d92b1ee7116cc769e0f3a31626e2d538d86a
Ameer Hamza <76493021+ameerhamza006@users.noreply.github.com>
2021-03-14 23:46:19 +0500

    How to input from user ?

---
+++ user-input.cs
@@ -0,0 +1,36 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            //for Question
+            Console.WriteLine("Enter Your Name");
+            //for User input
+            String name = Console.ReadLine();
+            //for print what user input
+            Console.WriteLine("Your Name is : "+ name); // concatinate with variable
+            //for Question
+            Console.WriteLine("Enter Your Age");
+            //for User input
+            String age = Console.ReadLine();
+            //for print what user input
+            Console.WriteLine("Your Age is : " + age); 
+            //for Question
+            Console.WriteLine("Enter Your Phone");
+            //for User input
+            var phone = Console.ReadLine();
+            //for print what user input
+            Console.WriteLine("Your Name Phone is :"+ phone);
+            //for Question
+            Console.WriteLine("Enter Your Password");
+            //for User input
+            var pass = Console.ReadLine();
+            //for print what user input
+            Console.WriteLine("Your Password is:"+pass);
+
+            //for holding Console
+            Console.ReadKey();
+
+            
+
+        }
+    }
