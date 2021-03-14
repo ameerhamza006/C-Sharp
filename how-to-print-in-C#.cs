40ae7bf8754a134650551072995a0ad352d965f6
Ameer Hamza <76493021+ameerhamza006@users.noreply.github.com>
2021-03-14 23:42:28 +0500

    How to Print in C# ?

---
+++ how-to-print-in-C#.cs
@@ -0,0 +1,13 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+              //for print
+            Console.WriteLine("Ameer Hamza");
+
+            //for holding Console
+            Console.ReadKey();
+        
+        }
+        
+    }
